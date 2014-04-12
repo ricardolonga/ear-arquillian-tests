7efbc8483e8047c34ee669d4c9aa5eaa9dfa7a80
mahnkong <mahnkong@gmx.de>
2014-04-12 20:54:50 +0200

    WHAT: wip

--- ear-arquillian-tests/test/src/test/java/mahnkong/testing/ear_arquillian_tests/impl/AbstractGreeterImplInsideEarTest.java
+++ ear-arquillian-tests/test/src/test/java/mahnkong/testing/ear_arquillian_tests/impl/AbstractGreeterImplInsideEarTest.java
@@ -11,7 +11,7 @@ public class AbstractGreeterImplInsideEarTest {
     @Deployment(testable = false, name="ear-arquillian-tests-ear.ear", order=1)
     public static EnterpriseArchive createEarDeployment() {
     	
-        File earFile = Maven.configureResolver().fromFile(new File("C:/tools/apache-maven-3.1.1/conf/settings.xml")).resolve("mahnkong.testing:ear-arquillian-tests-ear:ear:1.0").withoutTransitivity().asSingleFile();
+        File earFile = Maven.configureResolver().fromFile(new File("settings.xml")).resolve("mahnkong.testing:ear-arquillian-tests-ear:ear:1.0").withoutTransitivity().asSingleFile();
         EnterpriseArchive archive = ShrinkWrap.createFromZipFile(EnterpriseArchive.class, earFile);
 
         return archive;
