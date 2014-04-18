e33e255558966bf5b3cdbff35f484f555c2e717c
mahnkong <mahnkong@users.noreply.github.com>
2014-04-18 18:29:16 +0200

    Update DetachedTestMethod.java

--- ear-arquillian-tests/test/src/test/java/mahnkong/testing/ear_arquillian_tests/api/DetachedTestMethod.java
+++ ear-arquillian-tests/test/src/test/java/mahnkong/testing/ear_arquillian_tests/api/DetachedTestMethod.java
@@ -2,7 +2,7 @@ package mahnkong.testing.ear_arquillian_tests.api;
 
 import java.io.Serializable;
 
-//The functional interface for our lamda
+//The functional interface for our lambda
 public interface DetachedTestMethod<T> extends Serializable {
 	public T execute(Greeter greeter);
 }
